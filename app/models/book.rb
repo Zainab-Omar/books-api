697e6d41300fa812ee90e90e2c1bd94735f788ff
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-04-21 16:27:25 -0500

    add associations and validations and migrate my db

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+    has_many :comments
+    validates :title, :author, :img_url, presence: true
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ApplicationRecord
+    belongs_to :book
+    validates :text, :commenter, presence: true
 end
