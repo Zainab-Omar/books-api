8428b04637a8a188d078fc78555aed8c1f554bee
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-04-22 10:02:27 -0500

    create index, create, comment_params, set_book  methods for comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,29 @@
 class Api::V1::CommentsController < ApplicationController
+    before_action :set_book
+
+    def index
+        comments = @book.comments
+        render json: comments
+
+    end
+
+    def create
+        comment = @book.comments.new(comment_params)
+        if comment.save
+            render json: book
+        else
+            render json: {errors: comment.errors.full_messages}
+        end
+
+    end
+
+    private
+
+    def set_book
+        @book = Book.find(params[:book_id])
+    end
+
+    def comment_params
+        params.require(:comment).permit(:text, :commenter, :book_id)
+    end
 end
