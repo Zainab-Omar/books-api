5e5f47326bc9aef61299172fa85789f163d487ef
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-04-22 09:43:41 -0500

    create index, show, create, update methods for books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -5,4 +5,34 @@ class Api::V1::BooksController < ApplicationController
         render json: BookSerializer.new(books)
     end
 
+    def show
+        book = Book.find(params[:id])
+        render json: book
+    end
+
+    def create
+        book = Book.new(book_params)
+        if book.save
+            render json: book
+        else
+            render json: {errors: book.errors.full_messages}
+        end
+    end
+
+    def update
+        book = Book.find(params[:id])
+        book.update(book_params)
+        if book.save
+            render json: book
+        else
+         render json: {errors: book.errors.full_messages}
+        end
+    end
+
+    private
+    def book_params
+        params.require(:book).permit(:title, :author, :img_url, :likes)
+    end
+
+
 end
