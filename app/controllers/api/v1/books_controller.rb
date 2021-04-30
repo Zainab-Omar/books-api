9f6c79c9edc5fe1f19b0ae6d515e33b58895ac7c
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-04-29 19:17:20 -0500

    remove commented codes

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::BooksController < ApplicationController
 
     def index
         books = Book.all.order_by_id
-        # render json: BookSerializer.new(books)
         render json: books
     end
 
@@ -21,9 +20,7 @@ class Api::V1::BooksController < ApplicationController
     end
 
     def update
-    #  binding.pry
         book = Book.find(params[:id])
-        # book.update(book_params)
         if book.update(book_params)
             render json: book
         else
