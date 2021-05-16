5f2333d42893fd1233c659c2030ed8838a0b1c66
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-05-16 00:05:41 -0500

    build auth controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :auth
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
       resources :books do
         resources :comments
       end 
