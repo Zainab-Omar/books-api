1af44bdbe91a0715841ec369751237d740838b1c
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-06-21 13:46:42 -0500

    create destroy method in books controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::BooksController < ApplicationController
     end
 
     def create
-        # binding.pry
+        binding.pry
         book = Book.new(book_params)
         if book.save
             render json: book
@@ -28,6 +28,11 @@ class Api::V1::BooksController < ApplicationController
          render json: {errors: book.errors.full_messages}
         end
     end
+
+    def destroy
+        book = Book.find(params[:id])
+        book.destroy
+      end
 
     private
     def book_params
