2afb84b846686ce91210e40282464d7ceb34aef0
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-07-16 16:06:29 -0500

    make sure every thing up to date

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::AuthController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create 
-    
       @user = User.find_by(email: user_params[:email])
      
       if @user && @user.authenticate(user_params[:password])
