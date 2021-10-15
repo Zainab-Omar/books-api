a277ca8f89ca1394a34703d47f32097084d6c814
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-10-14 21:59:09 -0500

    add password_confirmation to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,19 +8,19 @@ class Api::V1::UsersController < ApplicationController
     end    
 
     def create
-        # binding.pry
+    #   binding.pry
         @user = User.create(user_params)
         if @user.valid?
             @token = encode_token(user_id: @user.id)
             render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
         else
-            render json: { error: 'failed to create user' }, status: :not_acceptable
+            render json: { message: @user.errors.full_messages }, status: :not_acceptable
         end 
     end
 
     private
 
     def user_params
-        params.require(:user).permit(:username, :email, :password)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 end
