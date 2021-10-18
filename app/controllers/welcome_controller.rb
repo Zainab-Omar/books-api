4ee4ec814b368c9adcda5159d9a80259212e87bb
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-10-18 15:29:30 -0500

    remove inline render

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
     skip_before_action :authorized, only: [:index]
 
     def index
+        render json: { message: 'Welcome to home page' }
     end
 end
